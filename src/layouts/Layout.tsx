61dd6b3f49da23897349c48ef25d6d00f87ce8a1
Jesse Emana <jesseemana@gmail.com>
2024-04-29 13:52:29 +0200

    CHORE: app layout complete

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,13 +1,13 @@
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+import Hero from '@/components/Hero';
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
 
 const Layout = ({ children, showHero = false }: { children: React.ReactNode, showHero?: boolean }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <Header />
       {showHero && <Hero />}
-      <div className="container mx-auto flex-1 py-10">
+      <div className='container mx-auto flex-1 py-10'>
         {children}
       </div>
       <Footer />
