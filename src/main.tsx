4cb206322d60d88ea65a04165a939742ad20b4b2
Jesse Emana <jesseemana@gmail.com>
2024-04-29 23:45:19 +0200

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import './global.css';
-import React from 'react';
 import App from './App';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-// import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
-import { QueryClient, QueryClientProvider } from 'react-query';
+
 import { Toaster } from 'sonner';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,8 +20,14 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
-        <App />
-        <Toaster visibleToasts={1} position='top-right' richColors />
+        <Auth0ProviderWithNavigate>
+          <App />
+          <Toaster 
+            visibleToasts={1} 
+            position='top-right' 
+            richColors 
+          />
+        </Auth0ProviderWithNavigate>
       </QueryClientProvider>
     </Router>
   </React.StrictMode>
