80ae65402b07607f7d087276940a2f1d56fc32c3
Jesse Emana <jesseemana@gmail.com>
2024-05-05 14:57:46 +0200

    user profile

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,32 @@
+import UserProfileForm from '@/components/forms/user-profile-form/UserProfileForm';
+import useGetUser from '@/hooks/useGetUser';
+import useUpdateUser from '@/hooks/useUpdateUser';
+
+const Profile = () => {
+  const { currentUser, isLoading: isGetUserLoading } = useGetUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateUser();
+
+  if (isGetUserLoading) return (
+    <div className='h-[540px]'>
+      <p className='pt-10'>Loading...</p>
+    </div>
+  )
+
+  if (!currentUser) return (
+    <div className='h-[540px]'>
+      <p className='pt-10 text-gray-600'>unable to load user profile!</p>
+    </div>
+  )
+
+  return (
+    <div className='pt-[70px]'>
+      <UserProfileForm 
+        onSave={updateUser} 
+        currentUser={currentUser} 
+        isLoading={isUpdateLoading} 
+      />
+    </div>
+  )
+}
+
+export default Profile;
