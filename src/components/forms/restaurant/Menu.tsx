388297d8ea5757f05861342d416503b2d8295a4c
Jesse Emana <jesseemana@gmail.com>
2024-05-07 16:25:36 +0200

    menu section complete

---
+++ src/components/forms/restaurant/Menu.tsx
@@ -0,0 +1,47 @@
+import { useFieldArray, useFormContext } from 'react-hook-form';
+import { FormDescription, FormField, FormItem } from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import MenuItemInput from './MenuItemInput';
+
+
+const Menu = () => {
+  const { control } = useFormContext();
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: 'menuItems'
+  })
+
+  return (
+    <div className='space-y-2'>
+      <>
+        <h1 className='capitalize font-semibold text-2xl'>cuisines</h1>
+        <FormDescription>
+          Create your menu and give each item a name and a price
+        </FormDescription>
+      </>
+      
+      <FormField control={control} name='menuItems' render={() => (
+        <FormItem>
+          {fields.map((_, index) => (
+            <MenuItemInput 
+              key={index} 
+              index={index} 
+              removeItem={() => remove(index)} 
+            />
+          ))}
+        </FormItem>
+      )} />
+
+      <Button 
+        type='button' 
+        className='capitalize' 
+        onClick={() => append({ name: '', price: '' })} 
+      >
+        add menu item
+      </Button>
+    </div>
+  )
+}
+
+export default Menu;
---
+++ src/components/forms/restaurant/MenuItemInput.tsx
@@ -0,0 +1,51 @@
+import { 
+  FormControl, 
+  FormField, 
+  FormItem, 
+  FormLabel, 
+  FormMessage, 
+} from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { useFormContext } from 'react-hook-form';
+
+
+const MenuItemInput = ({ index, removeItem }: { index: number, removeItem: () => void }) => {
+  const { control } = useFormContext();
+
+  return (
+    <div className='flex flex-row items-end gap-2'>
+      <FormField control={control} name={`menuItems.${index}.name`} render={({ field }) => (
+        <FormItem>
+          <FormLabel className='flex items-center gap-1'>
+            Name: <FormMessage />
+          </FormLabel>
+          <FormControl>
+            <Input {...field} placeholder='nsima & pork' className='bg-white' />
+          </FormControl>
+        </FormItem>
+      )} />
+
+      <FormField control={control} name={`menuItems.${index}.price`} render={({ field }) => (
+        <FormItem>
+          <FormLabel className='flex items-center gap-1'>
+            Price (MWK): <FormMessage />
+          </FormLabel>
+          <FormControl>
+            <Input {...field} placeholder='4,000' className='bg-white' />
+          </FormControl>
+        </FormItem>
+      )} />
+
+      <Button 
+        type='button' 
+        onClick={removeItem} 
+        className='bg-red-500 max-h-fit capitalize'
+      >
+        remove
+      </Button>
+    </div>
+  )
+}
+
+export default MenuItemInput;
