6477ba13e27f08d92db95f0cd6ab42edbb8847e0
Jesse Emana <jesseemana@gmail.com>
2024-05-10 18:32:47 +0200

    search component for the homepage

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,29 @@
+import { useNavigate } from 'react-router';
 import hero from '../assets/hero.jpg';
+import HomeSearch, { SearchForm } from './HomeSearch';
 
 const Hero = () => {
+  const navigate = useNavigate();
+
+  function handleSearch(searchValues: SearchForm) {
+    navigate({
+      pathname: `/search/${searchValues.searchQuery}`,
+    });
+  }
+
   return (
     <div id='hero'>
       <img 
         src={hero} 
         alt='hero image' 
-        className='w-full max-h-[600px] object-cover md:min-h-screen'
+        className='w-full object-cover min-h-screen lg:max-h-[600px] relative'
       />
+      <div className='grid place-items-center md:flex'>
+        <HomeSearch 
+          onSubmit={handleSearch} 
+          styles='absolute md:top-[30%] lg:top-[40%] lg:left-[3%] top-[20%]' 
+        />
+      </div>
     </div>
   )
 }
