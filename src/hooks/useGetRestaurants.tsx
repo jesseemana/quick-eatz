7485f43241fffd1c1994bae0e3552f53e39889e5
Jesse Emana <jesseemana@gmail.com>
2024-05-13 14:04:11 +0200

    get restaurant hook

---
+++ src/hooks/useGetRestaurants.tsx
@@ -0,0 +1,25 @@
+import { axios_instance } from '@/api/axios';
+import { Restaurant } from '@/types';
+import { useQuery } from 'react-query';
+
+const useGetRestaurant = (restaurant_id: string)=> {
+  async function getRestaurantById(): Promise<Restaurant | undefined> {
+    try {
+      const response = await axios_instance.get(`/api/restaurant/${restaurant_id}`);
+      return response.data;
+    } catch (error: unknown) {
+      // @ts-expect-error might/might not be axios error
+      console.error('Error fetching restaurants:', error.message);
+    }
+  }
+
+  const { data: results, isLoading } = useQuery(
+    'fetchRestaurant', 
+    getRestaurantById, 
+    { enabled: !!restaurant_id }
+  );
+
+  return { results, isLoading, }
+}
+
+export default useGetRestaurant;
