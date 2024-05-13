483bc451a1dfc236245a34e900e48fda455f9cad
Jesse Emana <jesseemana@gmail.com>
2024-05-13 14:11:49 +0200

    rendering static ui even in loading state

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import useSearchRestaurant from '@/hooks/useSearchRestaurant';
+import { SearchForm } from '@/schemas/search';
 import SearchBar from '@/components/SearchBar';
 import PaginationSelector from '@/components/Pagination';
 import SearchResultInfo from '@/components/SearchResultInfo';
 import SearchResultCard from '@/components/SearchResultCard';
+import useSearchRestaurants from '@/hooks/useSearchRestaurants';
+
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
 
 
 const SearchPage = () => {
@@ -14,45 +23,82 @@ const SearchPage = () => {
     searchQuery: '',
     page: 1,
     selectedCuisines: [],
-    sortOption: 'bestMatch'
+    sortOption: 'bestMatch',
   })
 
-  const { results, isLoading, } = useSearchRestaurant(city);
+  const { results, isLoading, } = useSearchRestaurants(searchState, city);
 
-  if (isLoading) return <p className='pt-10 min-h-[500px]'>loading...</p>
+  function setPage(page: number) {
+    setSearchState(prev => ({
+      ...prev,
+      page,
+    }))
+  }
 
-  // if (!results?.data || !city) return <p className='pt-10 min-h-[500px]'>No results found</p>
+  function setSearchQuery(data: SearchForm) {
+    setSearchState(prev => ({
+      ...prev,
+      searchQuery: data.searchQuery,
+      page: 1,
+    }));
+  }
+
+  function resetSearch() {
+    setSearchState(prev => ({
+      ...prev,
+      searchQuery: '',
+      page: 1,
+    }));
+  }
 
   return (
     <div className='min-h-screen pt-16 grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
-      <div id='cuisine-list' className='border border-red-400'>
+      <div id='cuisine-list' className='border border-red-400 p-1'>
         cuisines
       </div>
-      <div id='main-content' className='border border-red-400'>
+      <div id='main-content' className='border border-red-400 p-1'>
         <SearchBar 
+          onReset={resetSearch} 
+          onSubmit={setSearchQuery}
           searchQuery={searchState.searchQuery}
-          // onSubmit={setSearchQuery}
-          // onReset={resetSearch} 
         />
-        <div className='flex justify-between flex-col gap-3 lg:flex-row'>
-          <SearchResultInfo 
-            city={city} 
-            total={results?.pagination.total} 
-          />
-        </div>
+         
+        {isLoading ? <p>loading...</p> : (
+          <>
+            <div className='flex justify-between flex-col gap-3 lg:flex-row'>
+              {!results || !city ? (
+                <SearchResultInfo 
+                  total={0} 
+                  city={city} 
+                />
+              ):(
+                <SearchResultInfo 
+                  city={city} 
+                  total={results.pagination.total} 
+                />
+              )}
+            </div>
+          
+            {!results || !city ? (
+              <p className='text-center py-20'>No restaurants to display</p>
+            ):(
+              results.data.map(restaurant => (
+                <SearchResultCard 
+                  key={restaurant._id} 
+                  restaurant={restaurant} 
+                />
+              ))
+            )}
+          </>
+        )}
 
-        {results?.data.map((restaurant) => (
-          <SearchResultCard 
-            key={restaurant._id} 
-            restaurant={restaurant} 
+        {results && city && 
+          <PaginationSelector 
+            onPageChange={setPage}
+            page={results.pagination.page}
+            pages={results.pagination.pages}
           />
-        ))}
-
-        <PaginationSelector 
-          page={results?.pagination.page}
-          pages={results?.pagination.pages}
-          // onPageChange={}
-        />
+        }
       </div>
     </div>
   );
