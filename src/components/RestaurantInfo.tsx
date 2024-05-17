32bd73ec143f082011b3cd6d35975742892b907c
Jesse Emana <jesseemana@gmail.com>
2024-05-17 17:11:11 +0200

    updated jsx for restaurant info

--- src/components/RestaurantInfo.tsx
+++ src/components/RestaurantInfo.tsx
@@ -1,30 +1,42 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
 import { Dot } from 'lucide-react';
 import { Restaurant } from '@/types';
+import { Separator } from './ui/separator';
+
 
 const RestaurantInfo = ({ restaurant }: { restaurant: Restaurant }) => {
   return (
-    <Card className='border-sla mt-8 rounded-sm w-full'>
-      <CardHeader>
-        <CardTitle className='text-xl md:text-3xl font-bold tracking-tight capitalize'>
-          {restaurant.restaurantName}
-        </CardTitle>
-        <CardDescription className='capitalize text-sm'>
-          {restaurant.city}, {restaurant.country}
-        </CardDescription>
-      </CardHeader>
-      <CardContent className='flex flex-wrap'>
-        {restaurant.cuisines.map((item, index) => (
-          <div 
-            key={item} 
-            className='flex'
-          >
-            <span className='capitalize'>{item}</span>
-            {index < restaurant.cuisines.length - 1 && <Dot />}
+    <div className='space-y-2 capitalize'>
+      <p className='md:text-4xl font-bold text-xl'>{restaurant.restaurantName}</p>
+      <p className='md:text-lg font-semibold'>
+        {restaurant.city}, {restaurant.country}
+      </p>
+      <div className='flex gap-8 flex-col md:flex-row'>
+        <p className='flex'>
+          {restaurant.cuisines.map((cuisine, index) => (
+            <span key={cuisine} className='flex'>
+              <span className='text-gray-600 text-[16px]'>{cuisine}</span>
+              {index < restaurant.cuisines.length - 1 && <Dot />}
+            </span>
+          ))}
+        </p>
+        <div className='border py-2 px-4 rounded-md ml-2 flex w-full justify-center md:justify-end'>
+          <div className='flex gap-4 md:items-end'>
+            <div className='flex flex-col items-center'>
+              <span className='font font-semibold'>delivery time</span>
+              <span className='text-gray-800 font-semibold'>
+                Mon - Sat: 
+                <span className='font-normal ml-1'>09:00AM - 10:00PM</span>
+              </span>
+            </div>
+            <Separator orientation='vertical' />
+            <div className='flex flex-col items-center'>
+              <span className='font-semibold text-sm'>delivery price</span>
+              <span className='text-gray-600 font-semibold'>${restaurant.deliveryPrice}</span>
+            </div>
           </div>
-        ))}
-      </CardContent>
-    </Card>
+        </div>
+      </div>
+    </div>
   );
 }
 
