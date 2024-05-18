737ef525d809a98c01a0a3a32581fbe41af8d0b1
Jesse Emana <jesseemana@gmail.com>
2024-05-19 00:06:59 +0200

    c:/Users/hey/Desktop/Foodieroo/client/src/components/RestaurantInfo.tsx

--- src/components/OrderSummary.tsx
+++ src/components/OrderSummary.tsx
@@ -26,54 +26,55 @@ const OrderSummary = ({ restaurant, cartItems, addToCart, decreaseCart, removeFr
   return (
     <>
       <CardHeader>
-        <CardTitle className='flex justify-between tracking-tight font-semibold text-2xl capitalize text-gray-700'>
-          <span>your order</span>
-          <span>${getTotalCost()}</span>
+        <CardTitle className='flex justify-between tracking-tight lg:text-2xl capitalize'>
+          <p>total order 
+            <span className='font font-normal'>(plus delivery)</span>
+          </p>
+          <p>${getTotalCost()}</p>
         </CardTitle>
         <Separator />
         <CardContent className='p-0'>
-          {cartItems.map(item => (
-            <div 
-              key={item._id} 
-              className='grid grid-cols-[4fr_2fr_2fr] place-items-center gap-4'
-            >
-              <span className='py-2'>
-                <span className='capitalize flex items-start'>{item.name}</span>
-              </span>
-              <div className='flex'>
-                <Minus 
-                  size={20} 
-                  className='cursor-pointer' 
-                  onClick={() => decreaseCart(item)}
-                />
-                <Badge 
-                  variant='outline' 
-                  className='mr-2 rounded-sm'
-                >
-                  {item.quantity}
-                </Badge>
-                <Plus 
-                  size={20} 
-                  onClick={() => addToCart(item)} 
-                  className='cursor-pointer' 
-                />
+          {cartItems.length === 0 
+          ? <p className='text text-center py-4'>choose meal(s) to order</p> 
+          : <>
+            {cartItems.map((item) => (
+              <div 
+                key={item._id} 
+                className='grid grid-cols-[4fr_2fr_2fr] place-items-center gap-4'
+              >
+                <p className='py-2'>
+                  <span className='capitalize flex items-start'>{item.name}</span>
+                </p>
+                <div className='flex'>
+                  <Minus 
+                    size={20} 
+                    className='cursor-pointer' 
+                    onClick={() => decreaseCart(item)}
+                  />
+                  <Badge 
+                    variant='outline' 
+                    className='mr-2 rounded-sm'
+                  >
+                    {item.quantity}
+                  </Badge>
+                  <Plus 
+                    size={20} 
+                    onClick={() => addToCart(item)} 
+                    className='cursor-pointer' 
+                  />
+                </div>
+                <div className='flex gap-2 flex-row-revers'>
+                  ${(item.price * item.quantity)}
+                  <Trash 
+                    color='red'
+                    size={20}
+                    className='cursor-pointer' 
+                    onClick={() => removeFromCart(item)}
+                  />
+                </div>
               </div>
-              <div className='flex gap-2 flex-row-revers'>
-                ${(item.price * item.quantity)}
-                <Trash 
-                  color='red'
-                  size={20}
-                  className='cursor-pointer' 
-                  onClick={() => removeFromCart(item)}
-                />
-              </div>
-            </div>
-          ))}
-          <Separator />
-          <div className='flex justify-between capitalize py-2 p-8'>
-            <span>delivery</span>
-            <span>${restaurant.deliveryPrice}</span>
-          </div>
+            ))}
+          </>}
           <Separator />
         </CardContent>
       </CardHeader>
