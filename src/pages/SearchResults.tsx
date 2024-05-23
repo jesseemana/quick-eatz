42ad83270df485f7792d786b3668d16f9014c658
Jesse Emana <jesseemana@gmail.com>
2024-05-23 23:30:51 +0200

    shrunk items for responsiveness

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,14 +1,21 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router';
-import useDocumentTitle from '@/hooks/useDocumentTitle';
-import Footer from '@/components/Footer';
-import { Button } from '@/components/ui/button';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Form, FormControl, FormField, FormItem } from '@/components/ui/form';
-import { useForm } from 'react-hook-form';
-import { Menu, Search } from 'lucide-react';
+import { SearchForm } from '@/schemas/search';
+import { Menu, MapPin } from 'lucide-react';
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import { Input } from '@/components/ui/input';
+import useDocumentTitle from '@/hooks/useDocumentTitle';
+import SearchBar from '@/components/SearchBar';
+// import Footer from '@/components/Footer';
+
+
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+  sortOption: string;
+};
 
 
 const SearchResults = () => {
@@ -16,70 +23,64 @@ const SearchResults = () => {
 
   useDocumentTitle('Search results');
 
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: '',
+    page: 1,
+    selectedCuisines: [],
+    sortOption: 'bestMatch'
+  })
+
   const { isAuthenticated, loginWithRedirect } = useAuth0();
 
-  const form = useForm();
-
-  function handleSubmit() {}
+  function handleSearch(data: SearchForm) {
+    setSearchState((prev) => ({
+      ...prev,
+      searchQuery: data.searchQuery,
+      page: 1
+    }));
+  }
 
   return (
-    <div className=''>
-      <header className='container p-3 flex items-center justify-between'>
-        <div className='flex items-center space-x-4'>
+    <div className='md:container'>
+      <header className='p-3 flex items-center justify-between'>
+        <div className='flex items-center md:space-x-2'>
           <Sheet>
             <SheetTrigger>
-              <Menu  />
+              <Menu />
             </SheetTrigger>
             <SheetContent side='left'>
+              // TODO: add navigation stuff right here
             </SheetContent>
           </Sheet>
           <Link 
             to='/' 
-            className='font-normal text-2xl'
+            className='font-normal text-lg md:text-2xl md:mb-2 mb-1'
           >
             quick<span className='font-semibold'>eatz</span>
           </Link>
         </div>
 
-        <div className='capitalize font-semibold'>{city}</div>
+        <div className='capitalize font-semibold flex items-center gap-1'>
+          <MapPin size={18} />
+          <span className='text-gray-600'>{city}</span>
+        </div>
 
-        <Form {...form}>
-          <form 
-            onSubmit={handleSubmit} 
-            className='flex items-center rounded-full px-4 w-[600px] bg-gray-100'
-          >
-            <Search 
-              className='text-gray-700' 
-            />
-            <FormField 
-              control={form.control}
-              name='searchQuery' 
-              render={({ field }) => (
-                <FormItem className='w-full'>
-                  <FormControl>
-                    <Input 
-                      {...field}
-                      className='border-none shadow-none focus-visible:ring-0' 
-                      placeholder='search by cuisine or restaurant'
-                      autoComplete='off'
-                    />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </form>
-        </Form>
+        <SearchBar 
+          onSubmit={handleSearch} 
+          searchQuery={searchState.searchQuery}
+          styles='hidden md:flex'
+        />
         
         {!isAuthenticated && (
-          <Button 
-          onClick={() => loginWithRedirect()} 
-            className='px-8 rounded-md bg-transparent border border-black text-black text-md shadow-none' 
+          <button 
+            onClick={() => loginWithRedirect()} 
+            className='px-6 py-1 rounded-full bg-transparent border border-black text-black text-md hover:text-white hover:bg-black hover:border-none' 
           >
             Login
-          </Button>
+          </button>
         )}
       </header>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   )
 }
