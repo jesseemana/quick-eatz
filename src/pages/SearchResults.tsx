022f1f19313cf53c54a7e6756a5bfa2e58d360bc
Jesse Emana <jesseemana@gmail.com>
2024-05-23 23:42:27 +0200

    added responsive searchbar

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -41,44 +41,52 @@ const SearchResults = () => {
   }
 
   return (
-    <div className='md:container'>
-      <header className='p-3 flex items-center justify-between'>
-        <div className='flex items-center md:space-x-2'>
-          <Sheet>
-            <SheetTrigger>
-              <Menu />
-            </SheetTrigger>
-            <SheetContent side='left'>
-              // TODO: add navigation stuff right here
-            </SheetContent>
-          </Sheet>
-          <Link 
-            to='/' 
-            className='font-normal text-lg md:text-2xl md:mb-2 mb-1'
-          >
-            quick<span className='font-semibold'>eatz</span>
-          </Link>
-        </div>
+    <div className='lg:container'>
+      <header className='p-3 grid space-y-6'>
+        <div className='flex items-center justify-between'>
+          <div className='flex items-center md:space-x-2'>
+            <Sheet>
+              <SheetTrigger>
+                <Menu />
+              </SheetTrigger>
+              <SheetContent side='left'>
+                // TODO: add navigation stuff right here
+              </SheetContent>
+            </Sheet>
+            <Link 
+              to='/' 
+              className='font-normal text-lg md:text-2xl md:mb-2 mb-1'
+            >
+              quick<span className='font-semibold'>eatz</span>
+            </Link>
+          </div>
 
-        <div className='capitalize font-semibold flex items-center gap-1'>
-          <MapPin size={18} />
-          <span className='text-gray-600'>{city}</span>
+          <div className='capitalize font-semibold flex items-center gap-1'>
+            <MapPin size={18} />
+            <span className='text-gray-600'>{city}</span>
+          </div>
+
+          <SearchBar 
+            onSubmit={handleSearch} 
+            searchQuery={searchState.searchQuery}
+            styles='hidden md:flex'
+          />
+
+          {!isAuthenticated && (
+            <button 
+              onClick={() => loginWithRedirect()} 
+              className='px-6 py-1 rounded-full bg-transparent border border-black text-black text-md hover:text-white hover:bg-black hover:border-none' 
+            >
+              Login
+            </button>
+          )}
         </div>
 
         <SearchBar 
           onSubmit={handleSearch} 
           searchQuery={searchState.searchQuery}
-          styles='hidden md:flex'
+          styles='md:hidden'
         />
-        
-        {!isAuthenticated && (
-          <button 
-            onClick={() => loginWithRedirect()} 
-            className='px-6 py-1 rounded-full bg-transparent border border-black text-black text-md hover:text-white hover:bg-black hover:border-none' 
-          >
-            Login
-          </button>
-        )}
       </header>
       {/* <Footer /> */}
     </div>
