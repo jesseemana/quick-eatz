70b008e4992bc55084b750fb0000346d616a8b0b
Jesse Emana <jesseemana@gmail.com>
2024-05-28 00:01:39 +0200

    refactored skeleton loaders using map

--- src/components/RestaurantLoader.tsx
+++ src/components/RestaurantLoader.tsx
@@ -4,7 +4,7 @@ import { AspectRatio } from './ui/aspect-ratio';
 
 const RestaurantLoader = () => {
   return (
-    <div className='space-y-4 container mt-2'>
+    <div className='space-y-4 container mt-2 mb-8'>
       <AspectRatio ratio={16/4}>
         <Skeleton className='h-full bg-gray-100 rounded-none' />
       </AspectRatio>
@@ -13,15 +13,15 @@ const RestaurantLoader = () => {
       <Skeleton className='rounded-none md:h-6 h-4 w-[30%] bg-gray-100'/>
       <Separator />
       <div className='space-y-4'>
-        <Skeleton className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100'/>
-        <Skeleton className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100'/>
-        <Skeleton className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100'/>
-        <Skeleton className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100'/>
-        <Skeleton className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100'/>
-        <Skeleton className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100'/>
+        {[...new Array(6)].map((_, index) => (
+          <Skeleton 
+            key={index} 
+            className='rounded-none md:h-40 h-20 md:w-[40%] w-[60%] bg-gray-100' 
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default RestaurantLoader;
