e1a3d72f9aa949328a8972786ebb685fe556f7d6
Jesse Emana <jesseemana@gmail.com>
2024-05-29 17:08:55 +0200

    changed homepage layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,40 @@
 import landingImage from '../assets/landing.png';
 import appDownloadImage from '../assets/appDownload.png';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero';
+import Header from '@/components/Header';
 
 const HomePage = () => {
   useDocumentTitle('Quick Eatz - Your Next Meal Delivered At Your At Your Door');
   return (
-    <div className='flex flex-col gap-12'>
-      <div className='grid md:grid-cols-2 gap-5'>
-        <img 
-          src={landingImage} 
-          alt='application landing page image'
-          className='h-auto w-auto'
-        />
-        <div className='flex flex-col items-center justify-center gap-4 text-center'>
-          <p className='font-bold text-3xl tracking-tighter text-gray-900 capitalize'>
-            order takeaway even faster!
-          </p>
-          <p className='text-gray-700'>
-            Download the Quick Eatz app for faster ordering and personalised recommendations.
-          </p>
+    <>
+      <Header />
+      <Hero />
+      <div className='flex flex-col gap-12'>
+        <div className='grid md:grid-cols-2 gap-5'>
           <img 
-            src={appDownloadImage} 
-            alt='app download image' 
+            src={landingImage} 
+            alt='application landing page image'
             className='h-auto w-auto'
           />
+          <div className='flex flex-col items-center justify-center gap-4 text-center'>
+            <p className='font-bold text-3xl tracking-tighter text-gray-900 capitalize'>
+              order takeaway even faster!
+            </p>
+            <p className='text-gray-700'>
+              Download the Quick Eatz app for faster ordering and personalised recommendations.
+            </p>
+            <img 
+              src={appDownloadImage} 
+              alt='app download image' 
+              className='h-auto w-auto'
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 };
 
