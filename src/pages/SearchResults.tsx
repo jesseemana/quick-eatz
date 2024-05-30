fe2efb59d9af6f1f32ca2e843659ae43f31d58c4
Jesse Emana <jesseemana@gmail.com>
2024-05-30 03:06:25 +0200

    created cuisines filter

--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -5,8 +5,9 @@ import { useSearchState } from '@/context/SearchQueryProvider';
 import SearchLoading from '@/components/SearchLoading';
 import SortDropdown from '@/components/SortDropdown';
 import BestMatch from '@/components/BestMatch';
-import PaginationSelector from '@/components/Pagination';
+import Cuisines from '@/components/Cuisines';
 import SearchResultCard from '@/components/SearchResultCard';
+import PaginationSelector from '@/components/Pagination';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
 import useSearchRestaurants from '@/hooks/useSearchRestaurants';
 import { restaurants } from '@/constants/constants';
@@ -15,7 +16,7 @@ import { restaurants } from '@/constants/constants';
 const SearchResults = () => {
   const { city } = useParams();
   const { setCity } = useCity();
-  const { searchState, setPage, setSortOption, } = useSearchState();
+  const { searchState, setPage, setSortOption, setSelectedCuisines } = useSearchState();
   const { results, isLoading } = useSearchRestaurants(searchState, city);
   const [checked, setChecked] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
@@ -27,25 +28,31 @@ const SearchResults = () => {
   }, [city, setCity]);
 
   const toggleBestMatch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked((prev) => !prev)
+    setChecked((prev) => !prev);
     setSortOption(e.target.value);
   }
 
   return (
-    <div className='md:container mx-auto'>
+    <div className='md:container mx-auto space-y-4'>
+      <div className='space-y-4 px-2'>
+        <Cuisines 
+          onChange={setSelectedCuisines} 
+          selectedCuisines={searchState.selectedCuisines} 
+        />
+        <div className='flex items-center'>
+          <BestMatch 
+            checked={checked} 
+            toggleBestMatch={toggleBestMatch} 
+          />
+          <SortDropdown 
+            isExpanded={isExpanded} 
+            onChange={(value) => setSortOption(value)} 
+            onExpand={() => setIsExpanded((prev) => !prev)} 
+          />
+        </div>
+      </div>
       {isLoading ? <SearchLoading /> : (
         <div className='space-y-4'>
-          <div className='flex items-center px-2'>
-            <BestMatch 
-              checked={checked} 
-              toggleBestMatch={toggleBestMatch} 
-            />
-            <SortDropdown 
-              isExpanded={isExpanded} 
-              onChange={(value) => setSortOption(value)} 
-              onExpand={() => setIsExpanded((prev) => !prev)} 
-            />
-          </div>
           <p className='px-2 text-gray-800 font-semibold lg:text-2xl text-xl'>
             Results for <span className='capitalize'>{city}</span>
           </p>
