4de305b3fb4ab836734dfa6c338ebda8f04971ad
Jesse Emana <jesseemana@gmail.com>
2024-05-31 22:50:08 +0200

    modified nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,34 @@
 import { Link } from 'react-router-dom';
-import MainNav from './navbar/MainNav';
-import MobileNav from './navbar/MobileNav';
+import Nav from './navbar/Nav';
+import { Button } from './ui/button';
+import { useAuth0 } from '@auth0/auth0-react';
 
-const Header = ({ styles }: { styles?: string }) => {
+
+const Header = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
-    <header className={`fixed w-full py-3 z-10 ${styles}`}>
+    <header className='fixed w-full py-3 z-10'>
       <nav className='container mx-auto flex justify-between items-center'>
-        <Link
-          to='/'
-          className='text-2xl font-light tracking-tight text-white'
-        >
-          quickeatz
-        </Link>
-        <div className='md:hidden'>
-          <MobileNav />
+        <div className='flex items-center gap-2'>
+          <Nav className='text-white' />
+          <Link
+            to='/'
+            className='text-2xl font-light tracking-tight text-white mb-2'
+          >
+            quickeatz
+          </Link>
         </div>
-        <div className='hidden md:block'>
-          <MainNav />
+        <div className='flex gap-2'>
+          {!isAuthenticated && 
+            <Button 
+              variant='outline' 
+              onClick={async () => await loginWithRedirect()}
+              className='capitalize font-light border border-white bg-transparent text-white hover:text-black hover:bg-white'
+            >
+              log in
+            </Button>
+          }
         </div>
       </nav>
     </header>
