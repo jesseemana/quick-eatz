56e6617ee06ca8a82e0cb73b4344a3bed4bc24b4
Jesse Emana <jesseemana@gmail.com>
2024-05-31 22:48:44 +0200

    fixed positioning

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,14 +14,16 @@ const Hero = () => {
 
   return (
     <div id='hero'>
-      <img 
-        src={hero} 
-        alt='hero image' 
-        className='w-full object-cover min-h-screen lg:max-h-[600px]'
-        loading='lazy'
-      />
-      <div className='absolute md:top-[35%] top-[25%] left-5 flex items-center justify-center'>
-        <HomeSearch onSubmit={handleSearch} />
+      <div className='relative'>
+        <img 
+          src={hero} 
+          alt='hero image' 
+          className='w-full object-cover min-h-screen lg:max-h-[600px]'
+          loading='lazy'
+        />
+        <div className='absolute md:top-[35%] top-[25%] left-7'>
+          <HomeSearch onSubmit={handleSearch} />
+        </div>
       </div>
     </div>
   )
