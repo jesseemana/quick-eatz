bcf2e634720c3a7925c4d9c2d6769e082b940989
Jesse Emana <jesseemana@gmail.com>
2024-06-02 23:07:34 +0200

    edit user profile page

---
+++ src/pages/Edit.tsx
@@ -0,0 +1,35 @@
+import UserProfileForm from '@/components/forms/user-profile-form/UserProfileForm';
+import useDocumentTitle from '@/hooks/useDocumentTitle';
+import useGetUser from '@/hooks/useGetUser';
+import useUpdateUser from '@/hooks/useUpdateUser';
+
+const Edit = () => {
+  const { currentUser, isLoading: isGetUserLoading } = useGetUser();
+  const { updateUser, isLoading: isUpdateLoading } = useUpdateUser();
+  
+  useDocumentTitle('Edit profile');
+
+  if (isGetUserLoading) return (
+    <div className='h-[540px]'>
+      <p className='px-4'>Loading...</p>
+    </div>
+  )
+
+  if (!currentUser) return (
+    <div className='h-[540px]'>
+      <p className='px-4 text-gray-600'>unable to load user profile!</p>
+    </div>
+  )
+
+  return (
+    <div className='px-4'>
+      <UserProfileForm 
+        onSave={updateUser} 
+        currentUser={currentUser} 
+        isLoading={isUpdateLoading} 
+      />
+    </div>
+  )
+}
+
+export default Edit;
