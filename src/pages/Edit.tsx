a66d28d824843ba918ee7ea5158eb1aa89145907
Jesse Emana <jesseemana@gmail.com>
2024-06-04 14:14:35 +0200

    linnting stuff

--- src/pages/Edit.tsx
+++ src/pages/Edit.tsx
@@ -13,23 +13,23 @@ const Edit = () => {
     <div className='h-[540px]'>
       <p className='px-4'>Loading...</p>
     </div>
-  )
+  );
 
   if (!currentUser) return (
     <div className='h-[540px]'>
       <p className='px-4 text-gray-600'>unable to load user profile!</p>
     </div>
-  )
+  );
 
   return (
-    <div className='px-4 py-4'>
+    <div className='p-4'>
       <UserProfileForm 
         onSave={updateUser} 
         currentUser={currentUser} 
         isLoading={isUpdateLoading} 
       />
     </div>
-  )
+  );
 }
 
 export default Edit;
